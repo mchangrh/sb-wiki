d0e217dac73472c0d4cd5ea270f59f07c71aae41
Michael C <michael@mchang.name>
2023-10-22 20:38:12 -0400

    remove exceptions Namespace

--- mediawiki/settings/includes/Namespaces.php
+++ mediawiki/settings/includes/Namespaces.php
@@ -1,7 +1,2 @@
 <?php
-# add "Exceptions" Namespace
-define("NS_EXCEPTIONS", 3000);
-define("NS_EXCEPTIONS_TALK", 3001);
-$wgExtraNamespaces[NS_EXCEPTIONS] = "Exceptions";
-$wgExtraNamespaces[NS_EXCEPTIONS_TALK] = "Exceptions_talk";
 ?>
