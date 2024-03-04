e6a857b45b8f7fb4ad8872a09ed262eb9f3b0849
Michael C <michael@mchang.name>
2024-03-04 16:02:35 -0500

    add tokenizer and compose workflow

--- mediawiki/mw_add.sh
+++ mediawiki/mw_add.sh
@@ -6,7 +6,4 @@ git clone "https://gerrit.wikimedia.org/r/mediawiki/$type/$name"
 cd "$name" || exit
 git checkout "$MEDIAWIKI_GIT"
 rm -rf .git
-if [ -f composer.json ]; then
-  composer install --no-dev --prefer-dist --optimize-autoloader
-fi
 cd ../
