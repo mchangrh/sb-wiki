45756951f5d1d727e83dbd38e26d7d36b1b7e92a
Michael C <michael@mchang.name>
2025-01-30 02:10:20 +0800

    Explicitly mention CC 4.0

--- mediawiki/settings/includes/License.php
+++ mediawiki/settings/includes/License.php
@@ -1,6 +1,6 @@
 <?php
 $wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
 $wgRightsUrl = "https://creativecommons.org/licenses/by-sa/4.0/";
-$wgRightsText = "Creative Commons Attribution-ShareAlike";
+$wgRightsText = "Creative Commons Attribution-ShareAlike 4.0";
 $wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-sa.png";
 ?>
