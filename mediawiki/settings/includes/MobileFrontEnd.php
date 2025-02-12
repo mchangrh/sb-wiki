83c0cb4f52f2929852e110f3d372f18441032b76
Michael C <michael@mchang.name>
2025-02-12 15:38:59 -0500

    switch to minervia neue native night mode

--- mediawiki/settings/includes/MobileFrontEnd.php
+++ mediawiki/settings/includes/MobileFrontEnd.php
@@ -4,9 +4,10 @@ wfLoadExtension( 'MobileFrontend' );
 # mobile
 $wgDefaultMobileSkin = 'minerva';
 $wgMinervaDonateLink['base'] = false;
+$wgMinervaNightMode = ['base' => true ]
 $wgMFCollapseSectionsByDefault = false;
 # mobile dark theme
 $wgMFSiteStylesRenderBlocking = true;
 # use Mobile.css instead of Common.css (https://github.com/wikimedia/mediawiki-extensions-MobileFrontend?tab=readme-ov-file#wgmfcustomsitemodules)
-$wgMFCustomSiteModules = true;
+$wgMFCustomSiteModules = false;
 ?>
