41c39b0b69ace4cc87d3e24e0e937997fa300cdd
Michael C <michael@mchang.name>
2025-11-05 02:22:35 -0500

    add Spot_SponsorBlock

--- mediawiki/settings/includes/Namespaces.php
+++ mediawiki/settings/includes/Namespaces.php
@@ -1,2 +1,13 @@
 <?php
+# Spot_SponsorBlock
+# https://spotsponsorblock.org/
+define("NS_SPOTSPONSORBLOCK", 3002);
+define("NS_SPOTSPONSORBLOCK_TALK", 3003);
+
+$wgExtraNamespaces[NS_SPOTSPONSORBLOCK] = "Spot_SponsorBlock";
+$wgExtraNamespaces[NS_SPOTSPONSORBLOCK_TALK] = "Spot_SponsorBlock_talk";
+$wgNamespacesWithSubpages[NS_SPOTSPONSORBLOCK] = true;
+# End of Spot_SponsorBlock
+
+$wgContentNameSpaces[] = NS_SPOTSPONSORBLOCK;
 ?>
